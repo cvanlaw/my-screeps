c0231ed403ee6f4a5c93dff871a7e4e1314081c0
Chris Van Law <chris@chrisvanlaw.com>
2017-04-04 22:53:34 -0600

    fixed transporter to deliver resources once full

--- src/components/creeps/roles/transporter.ts
+++ src/components/creeps/roles/transporter.ts
@@ -1,3 +1,4 @@
+import {BaseWorker} from "./baseWorker";
 import { log } from "../../../lib/logger/log";
 /**
  * Runs all creep actions.
@@ -5,13 +6,14 @@ import { log } from "../../../lib/logger/log";
  * @export
  * @param {Creep} creep
  */
-export class Transporter {
+export class Transporter extends BaseWorker{
   public run(creep: Creep): void {
-
-
     let targetContainer = this.determineContainer(creep);
-
-    if (targetContainer) {
+    let spawn = creep.room.find<Spawn>(FIND_MY_SPAWNS)[0];
+    if(_.sum(creep.carry) == creep.carryCapacity) {
+      this.moveToDropEnergy(creep, spawn);
+    }
+    if (_.sum(creep.carry) < creep.carryCapacity && targetContainer) {
       if (creep.withdraw(targetContainer, RESOURCE_ENERGY, creep.carryCapacity) == ERR_NOT_IN_RANGE)
         creep.moveTo(targetContainer);
     }
