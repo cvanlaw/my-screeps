c0271692d0e5fb73e779161e06c1276658b71ba2
Chris Van Law <chris@chrisvanlaw.com>
2017-04-05 17:19:05 -0600

    remove health constraint from repair logic for tower and builder roles.

--- src/components/creeps/creepManager.ts
+++ src/components/creeps/creepManager.ts
@@ -55,7 +55,7 @@ export class CreepManager {
       })[0];
       if (tower) {
         let closestDamagedStructure = tower.pos.findClosestByRange<Structure>(FIND_STRUCTURES, {
-          filter: (structure: Structure) => structure.hits < structure.hitsMax *.75 && structure.structureType != STRUCTURE_WALL
+          filter: (structure: Structure) => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL
         });
         if (closestDamagedStructure) {
           tower.repair(closestDamagedStructure);
